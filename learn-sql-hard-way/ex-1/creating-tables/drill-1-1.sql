efe54850605b3852e2464e2b08b2692d51a719a2
Saad Mashkoor Siddiqui <saadmsiddiqui96@gmail.com>
2019-09-14 19:23:49 +0500

    Add Drills/Exercise for chapter 1

---
+++ learn-sql-hard-way/ex-1/creating-tables/drill-1-1.sql
@@ -0,0 +1,6 @@
+create table person (
+	id integer primary key, 
+	first_name text,
+	last_name text,
+	age integer,
+);
---
+++ learn-sql-hard-way/ex-1/creating-tables/drill-1-2.sql
@@ -0,0 +1,11 @@
+
+CREATE TABLE person (
+	id INTEGER PRIMARY KEY,
+	first_name TEXT,
+	middle_name TEXT,
+	last_name TEXT, 
+	address TEXT,
+	age INTEGER,
+   	weight REAL,
+	height REAL
+);
