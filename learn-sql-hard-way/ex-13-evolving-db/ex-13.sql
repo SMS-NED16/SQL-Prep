5f6ddf020b69ee286eda881a4f035d11fd328247
Saad Siddiqu <saadsiddiqui.ned16@gmail.com>
2019-09-19 15:35:03 +0500

    Update person table with dead, salary, and dob

--- learn-sql-hard-way/ex-13-evolving-db/ex-13.sql
+++ learn-sql-hard-way/ex-13-evolving-db/ex-13.sql
@@ -14,3 +14,23 @@ ALTER TABLE person_pet ADD COLUMN purchased_on DATETIME;
 /* Add a `parent` column to `pet` - Integer, holds id for this pet's parent */
 ALTER TABLE pet ADD COLUMN parent INTEGER;
 
+/* Use UPDATE statements to update the data for newly added columns */
+/* Dead, salary, and dob for `person`*/
+UPDATE person SET dead = 0; 			/* None of the entries are deceased */
+UPDATE person SET salary = 370000 WHERE 
+	first_name = 'Zed' AND;
+	last_name = 'Shaw';
+
+UPDATE person SET salary = 0 WHERE
+	first_name = 'Saad' AND
+	last_name = 'Siddiqui';
+
+UPDATE person SET dob = '31-01-1982' WHERE
+	first_name = 'Zed' AND
+	last_name = 'Shaw';
+
+UPDATE person SET dob = '06-05-1996' WHERE
+	first_name = 'Saad' AND
+	last_name = 'Siddiqui';
+
+	
